3c60e1e7c64309280a4793ee45b275877f1f2354
DESKTOP-HA97GMI\SSAFY <gnaao1005@gmail.com>
2025-07-09 17:09:18 +0900

    Initial to Forms&UI

---
+++ src/App.tsx
@@ -0,0 +1,76 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/layout";
+import Profile from "./routes/profile";
+import Home from "./routes/home";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import styled, { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./components/loading-screen";
+import { auth } from "./firebase";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
+]);
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color: white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const init = async () => {
+    // wait for firebase
+    await auth.authStateReady(); // 최초 인증 상태가 완료될 때 실행되는 Promise를 반환(firebase가 인증 확인 할 때까지 대기)
+    // setTimeout(() => setIsLoading(false), 2000); 2초 동안 로딩(하는척)
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    init();
+  }, []);
+  return (
+    <Wrapper>
+      <GlobalStyles />
+      {/* <RouterProvider router={router} /> */}
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
+    </Wrapper>
+  );
+}
+
+export default App;
---
+++ src/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDuwh4qf_BodhaKVVqbC-pFYUf76Pf4AkI",
+  authDomain: "nwitter-reloaded-368d0.firebaseapp.com",
+  projectId: "nwitter-reloaded-368d0",
+  storageBucket: "nwitter-reloaded-368d0.firebasestorage.app",
+  messagingSenderId: "215848869827",
+  appId: "1:215848869827:web:b68854845c8fe332c1a776",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
